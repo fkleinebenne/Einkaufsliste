12915445d208fdc1fe2fa1d4894885765a7eae71
fkleinebenne <friederike.kleinebenne@gmail.com>
2022-10-27 15:05:31 +0200

    Kommi hinzugefügt

--- app/src/main/java/de/fhbi/swe/einkaufsliste/MainActivity.java
+++ app/src/main/java/de/fhbi/swe/einkaufsliste/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         arad= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arrayList);
     }
-
+//alles funktioniert <3
     public void setAdapter(ListView listView){
         listView.setAdapter(arad);
     }
